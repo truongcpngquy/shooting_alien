c7e01af73f6a22767731f88087018ca172a3f850
congquy <congquy010206@gmail.com>
2025-11-17 08:49:04 +0700

    game

---
+++ ship.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Ship(Sprite):
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        # Make sure 'ship.bmp' is in the 'images' folder
+        self.image = pygame.image.load('images/ship.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (60, 60))
+        self.rect = self.image.get_rect()
+
+        self.center_ship()
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on movement flags."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = int(self.x)
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
